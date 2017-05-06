22561a2ed77061e3454e9c0d43c87b17bd6f3c06
onisac-K <799644275@qq.com>
2017-05-06 13:14:20 +0800

    first

---
+++ Academy/student/models.py
@@ -0,0 +1,62 @@
+ #-*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+import sys  
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+class Department(models.Model):
+    dno = models.CharField(max_length = 20)
+    dname = models.CharField(max_length = 20,null = False)
+    def __unicode__(self):
+        return '%s,%s'%(self.dno,self.dname)
+
+class Teacher(models.Model):
+    tno = models.CharField(max_length = 20)
+    tname = models.CharField(max_length = 20,null = False)
+    department = models.ForeignKey(Department)
+
+    def __unicode__(self):
+        return '%s,%s'%(self.tno,self.tname)
+
+class Student(models.Model):
+    sno = models.CharField(max_length = 20)
+    sname = models.CharField(max_length = 20,null = False)
+    department = models.ForeignKey(Department)
+    def __unicode__(self):
+        return '%s,%s'%(self.sno,self.sname)
+
+class Course(models.Model):
+    cno = models.CharField(max_length = 20)
+    cname = models.CharField(max_length = 20,null = False)
+    credit = models.IntegerField(null=False)
+    department = models.ForeignKey(Department)
+    def __unicode__(self):
+        return '%s'%(self.cno)
+
+class Opencourse(models.Model):
+    term = models.IntegerField(null=False)
+    course = models.ForeignKey(Course)
+    teacher = models.ForeignKey(Teacher)
+    time = models.CharField(max_length = 30,null = False)
+    class Meta:
+            unique_together = ('term','course','teacher')
+    primary = ('term','course','teacher')
+    def __unicode__(self):
+        return '%d,%s,%s'%(self.term,self.cno,self.tno)
+
+class Selection(models.Model):
+    student = models.ForeignKey(Student)
+    opencourse = models.ForeignKey(Opencourse)
+    usual = models.FloatField(null=False)
+    exam = models.FloatField(null=False)
+    total = models.FloatField(null=False)
+    class Meta:
+            unique_together = ('student','opencourse')
+    primary = ('student','opencourse')
+    def __unicode__(self):
+        return '%d,%d,%s,%s'%(self.sno,self.term,self.cno,self.tno)
+
+# Create your models here.
